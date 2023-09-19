2cddeb1855cb6541e81de529f3486fb73d249fb6
viki <gurmankaviki@gmail.com>
2023-09-19 22:18:19 +0200

    remove parallelism in State

--- WFA_Lib/HelperStructsForWFA.cs
+++ WFA_Lib/HelperStructsForWFA.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Extreme.DataAnalysis;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Globalization;
 using System.Threading.Tasks;
 
@@ -55,182 +58,177 @@ namespace WFA_Lib
             }
 
             double[,] image = new double[size, size];
-            var values = Values;
-            Parallel.For(0, size, (index) =>
+
+            for (int i = 0; i < size; i++)
             {
                 for (int j = 0; j < size; j++)
                 {
-                    image[index, j] = values[x + index, y + j];
+                    image[i, j] = Values[x + i, y + j];
                 }
-            });
-
-            return new StateImage(image, size);
             }
+            return new StateImage(image, size);
+        }
 
         public MyVector ToVector()
         {
             var vector = new MyVector(new double[Size * Size]);
 
-            var values = Values;
-            int size = Size;
-            Parallel.For(0, size, (index) =>
+            for (int i = 0; i < Size; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < Size; j++)
                 {
-                    vector.Values[index * size + j] = values[index, j];
+                    vector.Values[i * Size + j] = Values[i, j];
                 }
-            });
-
+            }
             return vector;
         }
     }
-
-    public struct State
-    {
-        public int ID { get; private set; }
-
-        public StateImage Image { get => Images[0]; }
-        List<StateImage> Images { get; }
-        public int HighestResolution { get; private set; }
-
-        private bool processed;
-        public bool Processed { get => processed; }
-
-        public State(int id, StateImage image)
+        public struct State
         {
-            ID = id;
-            Images = new List<StateImage>();
-            Images.Add(image);
-            HighestResolution = image.Size;
-            processed = false;
-        }
+            public int ID { get; private set; }
 
-        public StateImage GetImageWithSize(int newSize)
-        {
-            foreach (var image in Images)
+            public StateImage Image { get => Images[0]; }
+            List<StateImage> Images { get; }
+            public int HighestResolution { get; private set; }
+
+            private bool processed;
+            public bool Processed { get => processed; }
+
+            public State(int id, StateImage image)
             {
-                if (image.Size == newSize)
-                    return image;
+                ID = id;
+                Images = new List<StateImage>();
+                Images.Add(image);
+                HighestResolution = image.Size;
+                processed = false;
             }
-            double[,] img = ImageManipulator.Shrink(Images[0].Values, newSize, newSize);
-            StateImage newImage = new StateImage(img, newSize);
-            Images.Add(newImage);
-            return newImage;
-        }
 
-        public static State CreateProcessedState(int id, StateImage image)
-        {
-            State newState = new State(id, image);
-            newState.processed = true;
-
-            return newState;
-        }
-
-        public double GetAverageIntensity()
-        {
-            foreach (var img in Images)
+            public StateImage GetImageWithSize(int newSize)
             {
-                if (img.Size == 1)
+                foreach (var image in Images)
                 {
-                    return img.Values[0, 0];
+                    if (image.Size == newSize)
+                        return image;
                 }
+                double[,] img = ImageManipulator.Shrink(Images[0].Values, newSize, newSize);
+                StateImage newImage = new StateImage(img, newSize);
+                Images.Add(newImage);
+                return newImage;
             }
 
-            var smallestImg = Images[Images.Count - 1];
-            double average = 0;
-            for (int i = 0; i < smallestImg.Size; i++)
+            public static State CreateProcessedState(int id, StateImage image)
             {
-                for (int j = 0; j < smallestImg.Size; j++)
-                {
-                    average += smallestImg.Values[i, j];
-                }
+                State newState = new State(id, image);
+                newState.processed = true;
+
+                return newState;
             }
-            return average / (smallestImg.Size * smallestImg.Size);
-        }
-    }
 
-    public struct Transition
-    {
-        public int FromStateId { get; private set; }
-        public Alphabet Label { get; private set; }
-        public int ToStateId { get; private set; }
-        public double Weight { get; private set; }
-
-        public Transition(int fromStateId, int toStateId, Alphabet label, double weight)
-        {
-            FromStateId = fromStateId;
-            ToStateId = toStateId;
-            Label = label;
-            Weight = weight;
-        }
-
-        public override string ToString()
-        {
-            return $"{FromStateId}, {ToStateId}, {Label}, {Weight.ToString(CultureInfo.CreateSpecificCulture("en-US"))}";
-        }
-    }
-
-    public struct Coordinates
-    {
-        public int X { get; }
-        public int Y { get; }
-
-        public Coordinates(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public static explicit operator Coordinates(Word word)
-        {
-            int x = 0;
-            int y = 0;
-
-            int power = word.Values.Count;
-            int size = (int)Math.Pow(2, power - 1);
-
-            foreach (var letter in word.Values)
+            public double GetAverageIntensity()
             {
-                switch (letter)
+                foreach (var img in Images)
                 {
-                    case Alphabet._0:
-                        //x += size;
-                        y += size;
-                        size /= 2;
-                        break;
-                    case Alphabet._1:
-                        x += size;
-                        y += size;
-                        size /= 2;
-                        break;
-                    case Alphabet._2:
-                        //x += size;
-                        //y += size;
-                        size /= 2;
-                        break;
-                    case Alphabet._3:
-                        x += size;
-                        //y += size;
-                        size /= 2;
-                        break;
-                    default:
-                        break;
+                    if (img.Size == 1)
+                    {
+                        return img.Values[0, 0];
+                    }
                 }
+
+                var smallestImg = Images[Images.Count - 1];
+                double average = 0;
+                for (int i = 0; i < smallestImg.Size; i++)
+                {
+                    for (int j = 0; j < smallestImg.Size; j++)
+                    {
+                        average += smallestImg.Values[i, j];
+                    }
+                }
+                return average / (smallestImg.Size * smallestImg.Size);
+            }
+        }
+
+        public struct Transition
+        {
+            public int FromStateId { get; private set; }
+            public Alphabet Label { get; private set; }
+            public int ToStateId { get; private set; }
+            public double Weight { get; private set; }
+
+            public Transition(int fromStateId, int toStateId, Alphabet label, double weight)
+            {
+                FromStateId = fromStateId;
+                ToStateId = toStateId;
+                Label = label;
+                Weight = weight;
             }
 
-            return new Coordinates(x, y);
+            public override string ToString()
+            {
+                return $"{FromStateId}, {ToStateId}, {Label}, {Weight.ToString(CultureInfo.CreateSpecificCulture("en-US"))}";
+            }
         }
-    }
 
-    public struct ResolutionStruct
-    {
-        public int Width { get; private set; }
-        public int Height { get; private set; }
-
-        public ResolutionStruct(int width, int height)
+        public struct Coordinates
         {
-            Width = width;
-            Height = height;
+            public int X { get; }
+            public int Y { get; }
+
+            public Coordinates(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public static explicit operator Coordinates(Word word)
+            {
+                int x = 0;
+                int y = 0;
+
+                int power = word.Values.Count;
+                int size = (int)Math.Pow(2, power - 1);
+
+                foreach (var letter in word.Values)
+                {
+                    switch (letter)
+                    {
+                        case Alphabet._0:
+                            //x += size;
+                            y += size;
+                            size /= 2;
+                            break;
+                        case Alphabet._1:
+                            x += size;
+                            y += size;
+                            size /= 2;
+                            break;
+                        case Alphabet._2:
+                            //x += size;
+                            //y += size;
+                            size /= 2;
+                            break;
+                        case Alphabet._3:
+                            x += size;
+                            //y += size;
+                            size /= 2;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                return new Coordinates(x, y);
+            }
+        }
+
+        public struct ResolutionStruct
+        {
+            public int Width { get; private set; }
+            public int Height { get; private set; }
+
+            public ResolutionStruct(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
         }
     }
-}
